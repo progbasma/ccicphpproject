a8d8223ab12f7a23ba41d1a1b33d7d6a0cb03d53
Hossam Adel Elshami <hossamelshami2018@gmail.com>
2022-09-16 16:20:12 +0200

    update5

--- single-product.php
+++ single-product.php
@@ -1,5 +1,30 @@
 <?php
 include("includes/header.php");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecomm";
+
+if (isset($_GET["productid"])) {
+	$proid = $_GET["productid"];
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$sql = "SELECT products.id,products.name,products.description,products.price,products.photo,products.counter, category.name AS 'cate_name' FROM `products` INNER JOIN category ON products.category_id = category.id WHERE products.id=:productid;";
+		$stmt = $conn->prepare($sql);
+		$stmt->bindParam(':productid', $proid);
+
+		$stmt->execute();
+		$data = $stmt->fetch();
+	} catch (PDOException $e) {
+		$message = $e->getMessage();
+	}
+
+	$conn = null;
+}
 ?>
 <!--================Single Product Area =================-->
 <div class="product_image_area">
@@ -8,27 +33,41 @@ include("includes/header.php");
 			<div class="col-lg-6">
 				<div class="s_Product_carousel">
 					<div class="single-prd-item">
-						<img class="img-fluid" src="img/category/s-p1.jpg" alt="">
+						<img class="img-fluid" src="images/<?php echo $data['photo'];?>" alt="">
 					</div>
 					<div class="single-prd-item">
-						<img class="img-fluid" src="img/category/s-p1.jpg" alt="">
+						<img class="img-fluid" src="images/<?php echo $data['photo'];?>" alt="">
 					</div>
 					<div class="single-prd-item">
-						<img class="img-fluid" src="img/category/s-p1.jpg" alt="">
+						<img class="img-fluid" src="images/<?php echo $data['photo'];?>" alt="">
 					</div>
 				</div>
 			</div>
 			<div class="col-lg-5 offset-lg-1">
 				<div class="s_product_text">
-					<h3>Faded SkyBlu Denim Jeans</h3>
-					<h2>$149.99</h2>
+					<h3><?php echo $data['name'];?></h3>
+					<h2>$<?php echo $data['price'];?></h2>
 					<ul class="list">
-						<li><a class="active" href="#"><span>Category</span> : Household</a></li>
-						<li><a href="#"><span>Availibility</span> : In Stock</a></li>
+						<li><a class="active" href="#"><span>Category</span> : <?php echo $data["cate_name"];?></a></li>
+						<li><a href="#"><span>Availibility</span> 
+						<?php 
+						if( $data['counter'] > 0):
+						?>
+						<span style="color: green">
+						: In Stock
+						</span>
+						<?php 
+						else:
+						?>
+						<span style="color: red">
+						: Out Of Stock
+						</span>
+						<?php 
+						endif;
+						?>
+					</a></li>
 					</ul>
-					<p>Mill Oil is an innovative oil filled radiator with the most modern technology. If you are looking for
-						something that can make your interior look awesome, and at the same time give you the pleasant warm feeling
-						during the winter.</p>
+					<?php echo $data["description"]?>
 					<div class="product_count">
 						<label for="qty">Quantity:</label>
 						<input type="text" name="qty" id="sst" maxlength="12" value="1" title="Quantity:" class="input-text qty">
@@ -52,312 +91,12 @@ include("includes/header.php");
 	<div class="container">
 		<ul class="nav nav-tabs" id="myTab" role="tablist">
 			<li class="nav-item">
-				<a class="nav-link" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Description</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Specification</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Comments</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link active" id="review-tab" data-toggle="tab" href="#review" role="tab" aria-controls="review" aria-selected="false">Reviews</a>
-			</li>
+				<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Description</a>
+			</li>		
 		</ul>
 		<div class="tab-content" id="myTabContent">
-			<div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
-				<p>Beryl Cook is one of Britain’s most talented and amusing artists .Beryl’s pictures feature women of all shapes
-					and sizes enjoying themselves .Born between the two world wars, Beryl Cook eventually left Kendrick School in
-					Reading at the age of 15, where she went to secretarial school and then into an insurance office. After moving to
-					London and then Hampton, she eventually married her next door neighbour from Reading, John Cook. He was an
-					officer in the Merchant Navy and after he left the sea in 1956, they bought a pub for a year before John took a
-					job in Southern Rhodesia with a motor company. Beryl bought their young son a box of watercolours, and when
-					showing him how to use it, she decided that she herself quite enjoyed painting. John subsequently bought her a
-					child’s painting set for her birthday and it was with this that she produced her first significant work, a
-					half-length portrait of a dark-skinned lady with a vacant expression and large drooping breasts. It was aptly
-					named ‘Hangover’ by Beryl’s husband and</p>
-				<p>It is often frustrating to attempt to plan meals that are designed for one. Despite this fact, we are seeing
-					more and more recipe books and Internet websites that are dedicated to the act of cooking for one. Divorce and
-					the death of spouses or grown children leaving for college are all reasons that someone accustomed to cooking for
-					more than one would suddenly need to learn how to adjust all the cooking practices utilized before into a
-					streamlined plan of cooking that is more efficient for one person creating less</p>
-			</div>
-			<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-				<div class="table-responsive">
-					<table class="table">
-						<tbody>
-							<tr>
-								<td>
-									<h5>Width</h5>
-								</td>
-								<td>
-									<h5>128mm</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>Height</h5>
-								</td>
-								<td>
-									<h5>508mm</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>Depth</h5>
-								</td>
-								<td>
-									<h5>85mm</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>Weight</h5>
-								</td>
-								<td>
-									<h5>52gm</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>Quality checking</h5>
-								</td>
-								<td>
-									<h5>yes</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>Freshness Duration</h5>
-								</td>
-								<td>
-									<h5>03days</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>When packeting</h5>
-								</td>
-								<td>
-									<h5>Without touch of hand</h5>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<h5>Each Box contains</h5>
-								</td>
-								<td>
-									<h5>60pcs</h5>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="comment_list">
-							<div class="review_item">
-								<div class="media">
-									<div class="d-flex">
-										<img src="img/product/review-1.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4>Blake Ruiz</h4>
-										<h5>12th Feb, 2018 at 05:56 pm</h5>
-										<a class="reply_btn" href="#">Reply</a>
-									</div>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-									commodo</p>
-							</div>
-							<div class="review_item reply">
-								<div class="media">
-									<div class="d-flex">
-										<img src="img/product/review-2.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4>Blake Ruiz</h4>
-										<h5>12th Feb, 2018 at 05:56 pm</h5>
-										<a class="reply_btn" href="#">Reply</a>
-									</div>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-									commodo</p>
-							</div>
-							<div class="review_item">
-								<div class="media">
-									<div class="d-flex">
-										<img src="img/product/review-3.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4>Blake Ruiz</h4>
-										<h5>12th Feb, 2018 at 05:56 pm</h5>
-										<a class="reply_btn" href="#">Reply</a>
-									</div>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-									commodo</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6">
-						<div class="review_box">
-							<h4>Post a comment</h4>
-							<form class="row contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="text" class="form-control" id="name" name="name" placeholder="Your Full name">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="email" class="form-control" id="email" name="email" placeholder="Email Address">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="text" class="form-control" id="number" name="number" placeholder="Phone Number">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<textarea class="form-control" name="message" id="message" rows="1" placeholder="Message"></textarea>
-									</div>
-								</div>
-								<div class="col-md-12 text-right">
-									<button type="submit" value="submit" class="btn primary-btn">Submit Now</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="tab-pane fade show active" id="review" role="tabpanel" aria-labelledby="review-tab">
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="row total_rate">
-							<div class="col-6">
-								<div class="box_total">
-									<h5>Overall</h5>
-									<h4>4.0</h4>
-									<h6>(03 Reviews)</h6>
-								</div>
-							</div>
-							<div class="col-6">
-								<div class="rating_list">
-									<h3>Based on 3 Reviews</h3>
-									<ul class="list">
-										<li><a href="#">5 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> 01</a></li>
-										<li><a href="#">4 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> 01</a></li>
-										<li><a href="#">3 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> 01</a></li>
-										<li><a href="#">2 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> 01</a></li>
-										<li><a href="#">1 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> 01</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="review_list">
-							<div class="review_item">
-								<div class="media">
-									<div class="d-flex">
-										<img src="img/product/review-1.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4>Blake Ruiz</h4>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-									</div>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-									commodo</p>
-							</div>
-							<div class="review_item">
-								<div class="media">
-									<div class="d-flex">
-										<img src="img/product/review-2.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4>Blake Ruiz</h4>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-									</div>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-									commodo</p>
-							</div>
-							<div class="review_item">
-								<div class="media">
-									<div class="d-flex">
-										<img src="img/product/review-3.png" alt="">
-									</div>
-									<div class="media-body">
-										<h4>Blake Ruiz</h4>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-										<i class="fa fa-star"></i>
-									</div>
-								</div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-									commodo</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-6">
-						<div class="review_box">
-							<h4>Add a Review</h4>
-							<p>Your Rating:</p>
-							<ul class="list">
-								<li><a href="#"><i class="fa fa-star"></i></a></li>
-								<li><a href="#"><i class="fa fa-star"></i></a></li>
-								<li><a href="#"><i class="fa fa-star"></i></a></li>
-								<li><a href="#"><i class="fa fa-star"></i></a></li>
-								<li><a href="#"><i class="fa fa-star"></i></a></li>
-							</ul>
-							<p>Outstanding</p>
-							<form class="row contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="text" class="form-control" id="name" name="name" placeholder="Your Full name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Full name'">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="email" class="form-control" id="email" name="email" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address'">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="text" class="form-control" id="number" name="number" placeholder="Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Phone Number'">
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<textarea class="form-control" name="message" id="message" rows="1" placeholder="Review" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Review'"></textarea></textarea>
-									</div>
-								</div>
-								<div class="col-md-12 text-right">
-									<button type="submit" value="submit" class="primary-btn">Submit Now</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
+			<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+			<?php echo $data["description"]?>
 			</div>
 		</div>
 	</div>
